fc72ccb4495c34590889e20b37c30b8021184181
Damar2121 <damar.vivoy51@gmail.com>
2024-05-10 11:39:03 +0700

    jika stack nya tidak kosong

--- stackusinglinkedlist/stackusinglinkedlist.cpp
+++ stackusinglinkedlist/stackusinglinkedlist.cpp
@@ -39,3 +39,12 @@ public:
 		if (top == NULL) {
 			cout << "List is empty." << endl;
 		}
+
+		else {
+			Node* current = top;
+			while (current != NULL) {
+				cout << current->data << "   " << endl;
+				current = current->next;
+			}
+			cout << endl;
+		}
